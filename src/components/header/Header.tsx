530986344ec666d787c69bcd19f666b9d1aa09a1
Andres Ortiz <f.andresort@gmail.com>
2023-02-16 18:46:59 -0500

    style reset and basic components added

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header-wrapper">
+      <h1 className="header-title">Model Peril Timeline</h1>
+      <div className="header-hamburger">hamburger</div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/timeline/Timeline.tsx
@@ -0,0 +1,5 @@
+const Timeline = () => {
+  return <div></div>;
+};
+
+export default Timeline;
