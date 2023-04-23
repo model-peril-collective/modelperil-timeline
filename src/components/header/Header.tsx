1cb136b60c1515fbc48b0ed975307d10a6c70a12
Andres Ortiz <f.andresort@gmail.com>
2023-04-23 14:08:21 -0400

    images working 🙌
    - full page Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,21 @@
+import { classNames } from '../../utils/Utils';
 import './Header.css';
+import { useInView } from 'react-intersection-observer';
+
+const logo = require('../../assets/logo.png');
 
 const Header = () => {
+  const { ref, inView } = useInView({
+    /* Optional options */
+    threshold: 0.3,
+  });
+
   return (
-    <header className="header-wrapper">
-      <h1 className="header-title">Model Peril Timeline</h1>
-      <div className="header-hamburger">hamburger</div>
+    <header ref={ref} className={classNames('header-wrapper', inView ? 'visible' : 'hidden')}>
+      <img className="header-logo" src={logo} />
+      <h1 className="header-title">
+        <span className="header-name">Model Peril</span> Timeline
+      </h1>
     </header>
   );
 };
