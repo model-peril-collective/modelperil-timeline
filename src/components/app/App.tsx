e1b06a03f63e117ff1995f9e9fc83524c846ba70
Andres Ortiz <f.andresort@gmail.com>
2023-05-18 20:37:46 -0400

    add sass and .scss support

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,12 +1,12 @@
 import Header from '../header/Header';
 import Article, { Piece } from '../article/Article';
-import './App.css';
+import './App.scss';
 
 import timelineText from '../../content/timelineText.json';
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="app">
       <div className="content">
         <Header />
         {timelineText.articles.map((piece, index) => {
