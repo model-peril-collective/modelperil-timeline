30a7bb26db837f819c0d5eaa925d9e9120c811bf
Andres Ortiz <f.andresort@gmail.com>
2024-03-22 17:35:01 -0400

    adjust eslint rule for build folder

--- src/components/year/Year.tsx
+++ src/components/year/Year.tsx
@@ -1,4 +1,4 @@
-import { lazy, useEffect, useRef, useState } from 'react';
+import { lazy, useRef } from 'react';
 import { Story as StoryModel } from '../../models';
 import { ComponentFactory } from '..';
 import styles from './Year.module.scss';
@@ -14,27 +14,9 @@ const Year = (props: YearProps) => {
   const { stories, id } = props;
   const yearRef = useRef<HTMLDivElement>(null);
 
-  // const [scroll, setScroll] = useState(0);
-
   const getStoryId = (i: number) => {
     return 'year-' + id.toString() + '-' + i;
   };
-
-  // const onScroll = () => {
-  //   if (yearRef.current) {
-  //     setScroll(yearRef.current.scrollHeight)
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   window.addEventListener('scroll', onScroll);
-
-  //   return () => window.removeEventListener('scroll', onScroll);
-  // }, []);
-
-  // useEffect(() => {
-  //   console.log(scroll);
-  // }, [scroll])
 
   return (
     <section id={'year-' + id.toString()} ref={yearRef} className={styles.sectionWrapper}>
