da10f9493ed790b5719774a598968f68ef46a7e5
Andres Ortiz <f.andresort@gmail.com>
2024-04-07 11:58:22 -0400

    add media query for mobile

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,13 +7,13 @@ import { ArtifactType } from './artifact/Artifact';
  * Object which exports functions that import components for lazy loading
  */
 const ComponentFactory = {
-  ArticleAsync: () => import(/* webpackChunkName: Article */ './article/Article'),
-  ArtifactAsync: () => import(/* webpackChunkName: Artifact */ './artifact/Artifact'),
-  HeroAsync: () => import(/* webpackChunkName: Hero */ './hero/Hero'),
-  LineTreeAsync: () => import(/* webpackChunkName: LineTree */ './linetree/LineTree'),
-  StoryAsync: () => import(/* webpackChunkName: Story */ './story/Story'),
-  TimelineAsync: () => import(/* webpackChunkName: Timeline */ './timeline/Timeline'),
-  YearAsync: () => import(/* webpackChunkName: Year */ './year/Year'),
+  ArticleAsync: () => import(/* webpackChunkName: "Article" */ './article/Article'),
+  ArtifactAsync: () => import(/* webpackChunkName: "Artifact" */ './artifact/Artifact'),
+  HeroAsync: () => import(/* webpackChunkName: "Hero" */ './hero/Hero'),
+  LineTreeAsync: () => import(/* webpackChunkName: "LineTree" */ './linetree/LineTree'),
+  StoryAsync: () => import(/* webpackChunkName: "Story" */ './story/Story'),
+  TimelineAsync: () => import(/* webpackChunkName: "Timeline" */ './timeline/Timeline'),
+  YearAsync: () => import(/* webpackChunkName: "Year" */ './year/Year'),
 };
 
 export { ArtifactType, ComponentFactory };
