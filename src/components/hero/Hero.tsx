823438ef2d76a7ac0887083d9bd9f3d1bbfc2dbe
Andres Ortiz <f.andresort@gmail.com>
2025-02-26 21:31:54 -0500

    rename logo pngs and add logo words components

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -2,17 +2,50 @@ import clsx from 'clsx';
 import { ForwardedRef, HTMLAttributes, forwardRef } from 'react';
 import styles from './Hero.module.scss';
 
-import logo from '../../assets/logo.png';
+import whiteM from '../../assets/logo/white_M_logo.png';
+import whiteO from '../../assets/logo/white_O_logo.png';
+import whiteD from '../../assets/logo/white_D_logo.png';
+import whiteE from '../../assets/logo/white_E_logo.png';
+import whiteL from '../../assets/logo/white_L_logo.png';
+import yellowP from '../../assets/logo/yellow_P_logo.png';
+import yellowE from '../../assets/logo/yellow_E_logo.png';
+import yellowR from '../../assets/logo/yellow_R_logo.png';
+import yellowI from '../../assets/logo/yellow_I_logo.png';
+import yellowL from '../../assets/logo/yellow_L_logo.png';
 
-export type HeroProps = HTMLAttributes<HTMLDivElement>
+export type HeroProps = HTMLAttributes<HTMLDivElement>;
 
-const Hero = forwardRef(function Hero(
-  props: HeroProps,
-  ref: ForwardedRef<HTMLElement>
-) {
+const LogoModel = () => {
   return (
-    <header ref={ref} className={clsx(styles.wrapper, styles.visible)}>
-      <img className={styles.logo} src={logo} alt="Model Peril logo" />
+    <div className={clsx(styles.logoWord, styles.white)}>
+      <img className={clsx(styles.letter, styles.whiteM)} src={whiteM} alt="Model Peril logo" />
+      <img className={styles.letter} src={whiteO} alt="Model Peril logo" />
+      <img className={styles.letter} src={whiteD} alt="Model Peril logo" />
+      <img className={clsx(styles.letter, styles.whiteE)} src={whiteE} alt="Model Peril logo" />
+      <img className={styles.letter} src={whiteL} alt="Model Peril logo" />
+    </div>
+  );
+};
+
+const LogoPeril = () => {
+  return (
+    <div className={clsx(styles.logoWord, styles.yellow)}>
+      <img className={styles.letter} src={yellowP} alt="Model Peril logo" />
+      <img className={clsx(styles.letter, styles.yellowE)} src={yellowE} alt="Model Peril logo" />
+      <img className={styles.letter} src={yellowR} alt="Model Peril logo" />
+      <img className={styles.letter} src={yellowI} alt="Model Peril logo" />
+      <img className={styles.letter} src={yellowL} alt="Model Peril logo" />
+    </div>
+  );
+};
+
+const Hero = forwardRef(function Hero(props: HeroProps, ref: ForwardedRef<HTMLElement>) {
+  return (
+    <header ref={ref} className={clsx(styles.wrapper, styles.visible)} {...props}>
+      <div className={styles.logoWrapper}>
+        <LogoModel />
+        <LogoPeril />
+      </div>
       <h1 className={styles.title}>
         <span className={styles.name}>A Model Minority Yellow Peril Timeline</span>
       </h1>
