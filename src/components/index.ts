59ebd59259473356282b3d56872ea0512f55904d
Andres Ortiz <f.andresort@gmail.com>
2025-03-05 02:24:39 -0500

    cleanup old article component

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,7 +7,6 @@ import { ArtifactType } from './artifact/Artifact';
  * Object which exports functions that import components for lazy loading
  */
 const ComponentFactory = {
-  ArticleAsync: () => import(/* webpackChunkName: "Article" */ './article/Article'),
   ArtifactAsync: () => import(/* webpackChunkName: "Artifact" */ './artifact/Artifact'),
   FooterAsync: () => import(/* webpackChunkName: "Footer" */ './footer/Footer'),
   HeroAsync: () => import(/* webpackChunkName: "Hero" */ './hero/Hero'),
